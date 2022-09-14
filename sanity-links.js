40344229a3e98a1652731b7c4659e723acf84e80
Dani Amsalem <drummingdan@gmail.com>
2022-09-14 16:22:00 -0400

    Remove Unnecessary Code

--- sanity-links.js
+++ sanity-links.js
@@ -1,5 +1,4 @@
 import sanityClient from "@sanity/client";
-import fetch from "node-fetch";
 import csv from "csv-parser";
 import fs from "fs";
 
@@ -15,38 +14,6 @@ const client = sanityClient({
 });
 
 const slugsCsv = "./slugs-to-redirect.csv";
-
-/* 
-// Change slugs for array of posts to new ones from Steven
-const query = `*[_type == "blog" && slug.current == $slugParam][0] {
-    _id
-  }`;
-
-fs.createReadStream(slugsCsv)
-	.pipe(csv())
-	.on("data", (data) => {
-		const params = { slugParam: data.from };
-		const newSlug = data.to;
-
-		client
-			.fetch(query, params)
-			.then((postId) => {
-				return postId ? postId._id : "";
-			})
-			.then((postId) => {
-				client
-					.patch(postId)
-					.set({
-						slug: {
-							_type: "slug",
-							current: newSlug,
-						},
-					})
-					.commit();
-			});
-	});
- */
-
 
 // Create redirects for the same array of posts from date slugs to new slugs
 
@@ -75,16 +42,3 @@ fs.createReadStream(slugsCsv)
 			.unset([`redirectList[_key=="sqsp-redirect-${data.to}"]`])
 			.commit(); */
 	});
-
-/* 
-// Create new CSV of all post titles for CG
-const postTitles = await client.fetch(
-`*[_type == "blog"]{title}`
-);
-postTitles.forEach(({title}) => {
-    fs.appendFile("all-blog-titles.csv", `${title},\n`, (err) => {
-        if (err) {
-            console.error(err);
-        }
-    });
-}) */
